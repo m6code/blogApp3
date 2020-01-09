467646d627f97de1ced3fe6266d1148c83f6a439
BenjaminM6 <benjamynfrank101@gmail.com>
2020-01-09 22:08:57 +0100

    Use templates and template inheritance to return views (home and about) add some dummy data to show on home page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+		'author': 'Benjamz',
+		'title': 'Blog 1',
+		'content': 'First Post content',
+		'date_posted': 'January 9, 2020'
+	},
+
+	{
+		'author': 'Benjamz',
+		'title': 'Blog 2',
+		'content': 'Second Post content',
+		'date_posted': 'January 9, 2020'
+	},
+	{
+		'author': 'Benjamz',
+		'title': 'Blog 3',
+		'content': 'Third Post content',
+		'date_posted': 'January 9, 2020'
+	},
+
+]
+
 def home(request):
-	return HttpResponse("<h1>Blog Homepage</h1>")
+	context = {
+		'posts': posts
+	}
+	return render(request, "blog/home.html", context)
 
 def about(request):
-	return HttpResponse("<h1>About</h1>")
+	return render(request, 'blog/about.html', title="About Page")
