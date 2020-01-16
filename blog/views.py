b1ed5057ec12bd7743a683cbd991d02c3a10d01f
BenjaminM6 <benjamynfrank101@gmail.com>
2020-01-16 13:29:04 +0100

    Load posts from database on homepage

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts = [
-	{
-		'author': 'Benjamz',
-		'title': 'Blog 1',
-		'content': 'First Post content',
-		'date_posted': 'January 9, 2020'
-	},
-
-	{
-		'author': 'Benjamz',
-		'title': 'Blog 2',
-		'content': 'Second Post content',
-		'date_posted': 'January 9, 2020'
-	},
-	{
-		'author': 'Benjamz',
-		'title': 'Blog 3',
-		'content': 'Third Post content',
-		'date_posted': 'January 9, 2020'
-	},
-
-]
+from .models import Post
 
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, "blog/home.html", context)
 
