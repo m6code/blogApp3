eddea9a3e31d5c07474bd549aa974ef87ff86e7f
BenjaminM6 <benjamynfrank101@gmail.com>
2020-01-23 22:54:22 +0100

    Update Profile page to edit user profile username,password and image fix mobile navbar resize images on upload

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
 	if request.method == "POST":
@@ -12,11 +12,28 @@ def register(request):
 			form.save()
 			username = form.cleaned_data.get('username')
 			messages.success(request, f'Account created for {username}!')
-			return redirect('blog-home')
+			return redirect('login')
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):
-	return render(request, 'users/profile.html')
+	if request.method == 'POST':
+		u_form = UserUpdateForm(request.POST, instance=request.user)
+		p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+		
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			messages.success(request, f'Account updated successfully!')
+			return redirect('profile')
+	else:
+		u_form = UserUpdateForm(instance=request.user)
+		p_form = ProfileUpdateForm(instance=request.user.profile)
+
+	context = {
+		'u_form' : u_form,
+		'p_form' : p_form
+	}
+	return render(request, 'users/profile.html', context)
