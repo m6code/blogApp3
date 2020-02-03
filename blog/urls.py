f8f560b2fdc68526db20035e63ae1e8a42dcf504
M6code <benjamynfrank101@gmail.com>
2020-02-03 12:57:24 +0100

    Use Class based views (ListView & DetailView) to display post on homepage and view a single post entry

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from .views import PostListView, PostDetailView
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-def home(request):
-	context = {
-		'posts': Post.objects.all()
-	}
-	return render(request, "blog/home.html", context)
+# def home(request):
+# 	context = {
+# 		'posts': Post.objects.all()
+# 	}
+# 	return render(request, "blog/home.html", context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+	model = Post
+	
 
 def about(request):
 	return render(request, 'blog/about.html', {'title':'About Page'} )
