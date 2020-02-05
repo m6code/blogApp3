92190c67c6a8b2f0a61bc3cb4c8a016ebb5250a4
M6code <benjamynfrank101@gmail.com>
2020-02-05 13:47:58 +0100

    Set only authorized Users to create post using Mixin

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post
 
@@ -19,7 +20,7 @@ class PostDetailView(DetailView):
 	model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
 	model = Post
 	fields = ['title', 'content']
 
