fca4f29fae8e131e1a4456b134779054ea856ce5
M6code <benjamynfrank101@gmail.com>
2020-02-18 10:31:19 +0100

    Add link to view paginated pages

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ class PostListView(ListView):
 	template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
 	context_object_name = 'posts'
 	ordering = ['-date_posted']
-	paginate_by = 2
+	paginate_by = 10
 
 class PostDetailView(DetailView):
 	model = Post
